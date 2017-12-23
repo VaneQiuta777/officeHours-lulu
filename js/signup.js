b3d7219855823e16768f54e3b33cc1d2ee1c5fda
VaneQiuta777 <vanessa.colsan@gmail.com>
2017-12-23 11:37:11 -0500

    añadiendo ultimas carpetas

--- js/signup.js
+++ js/signup.js
@@ -1,15 +1,26 @@
 $(document).ready(function() {
   var $email = $('#inputEmail');
-  var $password = $('inputPassword');
+  var $password = $('#inputPassword');
+  var $chekbox = $('.checkbox');
   // asociando eventos a los inpits
   // $email.keydown(function() {
   //   console.log($email);
   // }); evento keydown
   $email.on('input', function(event) {
     // console.log(event.target.value); //conocemos el valor escrito en el input 
-    console.log($(this).val());  //conocemos el valor escrito en el input 
+    // console.log($(this).val());  conocemos el valor escrito en el input 
     var PATERNEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
-    console.log (PATERNEMAIL.test($(this).val()));
+    console.log(PATERNEMAIL.test($(this).val()));
   });
-  
+  $password.on('input', function(evento) {
+    console.log($(this).val());
+    if ($(this).val().length >= 6) {
+      alert('cumple');
+    } else {
+      alert('no cumple');
+    }
+    // var PATERNPASSWORD =  /^[0-9]+$/;
+    // console.log(PATERNPASSWORD.test($(this).val()));
+  });
+
 });
